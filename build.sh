d71339859606de6ea8e4c433eaadb452a60ea059
Boren Zhang <bobo1993324@gmail.com>
2014-05-01 21:51:41 -0400

    static build with sdl lib

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+if [[ ! -f lib/SDL-1.2.15/build/.libs/libSDL.a ]]; then
+	cd lib
+	tar xf SDL-1.2.15.tar.gz
+	cd SDL-1.2.15/
+	patch  -p 1 -f < ../SDL1.patch
+	./autogen.sh && ./configure && make
+	cd ../..
+fi
 mkdir build
 cd build
-cmake .. -DENABLE_QT=on
+cmake .. -DSDL_PATH=`pwd`/../lib/SDL-1.2.15/
 make
