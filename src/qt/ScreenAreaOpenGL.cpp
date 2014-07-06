af81ade847ca130f52e668126c2f3cc7ffb9b3f3
Boren Zhang <bobo1993324@gmail.com>
2014-07-05 17:52:59 -0700

    hehe2

--- src/qt/ScreenAreaOpenGL.cpp
+++ src/qt/ScreenAreaOpenGL.cpp
@@ -23,54 +23,56 @@ void ScreenAreaOpenGL::handleWindowChanged(QQuickWindow *win)
 }
 
 void ScreenAreaOpenGL::paint() {
-    if (!m_program) {
-        m_program = new QOpenGLShaderProgram();
-        m_program->addShaderFromSourceCode(QOpenGLShader::Vertex,
-                                           "attribute vec2 vertex;"
-                                           "varying vec2 coords;"
-                                           "void main() {"
-                                           "    coords = vertex.xy;"
-                                           "    gl_Position = vec4 (vertex.x, vertex.y, 0, 1.0);"
-                                           "}");
-        m_program->addShaderFromSourceCode(QOpenGLShader::Fragment,
-                                           "uniform sampler2D myTexture;"
-                                           "varying vec2 coords;"
-                                           "void main() {"
-                                           "    gl_FragColor = texture2D(myTexture, vec2 (coords.x * 0.5 + 0.5, coords.y * -0.5 + 0.5));"
-                                           "}");
+//    if (!m_program) {
+//        m_program = new QOpenGLShaderProgram();
+//        m_program->addShaderFromSourceCode(QOpenGLShader::Vertex,
+//                                           "attribute vec2 vertex;"
+//                                           "varying vec2 coords;"
+//                                           "void main() {"
+//                                           "    coords = vertex.xy;"
+//                                           "    gl_Position = vec4 (vertex.x, vertex.y, 0, 1.0);"
+//                                           "}");
+//        m_program->addShaderFromSourceCode(QOpenGLShader::Fragment,
+//                                           "uniform sampler2D myTexture;"
+//                                           "varying vec2 coords;"
+//                                           "void main() {"
+//                                           "    gl_FragColor = texture2D(myTexture, vec2 (coords.x * 0.5 + 0.5, coords.y * -0.5 + 0.5));"
+//                                           "}");
 
-        //TODO destroy this in context
-        m_texture = new QOpenGLTexture(QOpenGLTexture::Target2D);
-        m_texture->setFormat(QOpenGLTexture::RGB8_UNorm);
-        m_texture->setSize(241, 160);
-        m_texture->setMipLevels(m_texture->maximumMipLevels());
-        m_texture->allocateStorage();
+//        //TODO destroy this in context
+//        m_texture = new QOpenGLTexture(QOpenGLTexture::Target2D);
+//        m_texture->setFormat(QOpenGLTexture::RGB8_UNorm);
+//        m_texture->setSize(241, 160);
+//        m_texture->setMipLevels(m_texture->maximumMipLevels());
+//        m_texture->allocateStorage();
 
-        m_program->bindAttributeLocation("vertex", 0);
-        m_program->link();
-    }
-    m_texture->setData(0, QOpenGLTexture::BGRA, QOpenGLTexture::UInt8, pix, 0);
+//        m_program->bindAttributeLocation("vertex", 0);
+//        m_program->link();
+//    }
+//    m_texture->setData(0, QOpenGLTexture::BGRA, QOpenGLTexture::UInt8, pix, 0);
 
-//! [4] //! [5]
-    m_program->bind();
+////! [4] //! [5]
+//    m_program->bind();
 
-    m_program->enableAttributeArray(0);
+//    m_program->enableAttributeArray(0);
 
-    float values[] = {
-        -1, -1,
-        1, -1,
-        -1, 1,
-        1, 1
-    };
-    m_program->setAttributeArray(0, GL_FLOAT, values, 2);
-    unsigned int textureLocation = m_program->uniformLocation("myTexture");
-    m_texture->bind(textureLocation);
+//    float values[] = {
+//        -1, -1,
+//        1, -1,
+//        -1, 1,
+//        1, 1
+//    };
+//    m_program->setAttributeArray(0, GL_FLOAT, values, 2);
+//    unsigned int textureLocation = m_program->uniformLocation("myTexture");
+//    m_texture->bind(textureLocation);
 
     glViewport(this->property("positionFromLeft").toInt(),
                this->property("positionFromBottom").toInt(),
                width(), height());
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    m_program->disableAttributeArray(0);
-    m_program->release();
+//    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+//    m_program->disableAttributeArray(0);
+//    m_program->release();
 }
